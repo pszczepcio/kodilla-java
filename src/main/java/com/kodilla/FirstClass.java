4aa335135a44bec00ae8c1cda98ce23d4b09f54d
golfik2005 <spiotrek1985@gmail.com>
2018-11-07 21:35:20 +0100

    kodilla testing

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -1,4 +1,7 @@
 package com.kodilla;
 
 public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+    }
 }
