232250e10697daa975aa9e8ed1c1404555006eab
golfik2005 <spiotrek1985@gmail.com>
2018-11-24 14:35:56 +0100

    Zadanie 7.6
    Signed-off-by: golfik2005 <spiotrek1985@gmail.com>

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/portfolio/User.java
@@ -0,0 +1,41 @@
+package com.kodilla.stream.portfolio;
+
+import java.util.Objects;
+
+public final class User {
+    private final String userName;
+    private final String realName;
+
+    public User(final String userName, final String realName) {
+        this.userName = userName;
+        this.realName = realName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getUserName(), user.getUserName());
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getUserName());
+//    }
+}
