1211da169a53bd78b9fd4bdec801d6ba41eacf12
golfik2005 <spiotrek1985@gmail.com>
2019-01-25 22:50:49 +0100

    warcaby
    Signed-off-by: golfik2005 <spiotrek1985@gmail.com>

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/GoodPatternsMain.java
@@ -0,0 +1,10 @@
+package com.kodilla.good.patterns.challenges;
+
+public class GoodPatternsMain {
+    public static void main(String[] args) {
+        MovieStore.getMovies().entrySet().stream()
+                .flatMap(e -> e.getValue().stream())
+                .map(e -> e.toString() + " ! ")
+                .forEach(System.out::print);
+    }
+}
