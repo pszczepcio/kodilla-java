b563c7bdd43f145bba269bf8a20ea239ecf18e33
golfik2005 <spiotrek1985@gmail.com>
2019-02-04 13:06:24 +0100

    zadanie 14.4
    Signed-off-by: golfik2005 <spiotrek1985@gmail.com>

--- kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
+++ kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
@@ -1,12 +1,14 @@
-package com.kodilla;
+package com.kodilla.spring.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KodillaCourseApplication {
+public class KodillaSpringWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KodillaCourseApplication.class, args);
+        SpringApplication.run(KodillaSpringWebApplication.class, args);
     }
+
 }
+
