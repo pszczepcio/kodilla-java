2c91d91336bea912f22e76563d86c596d6b54e4f
golfik2005 <spiotrek1985@gmail.com>
2019-03-14 21:32:06 +0100

    sudoku 14.03
    Signed-off-by: golfik2005 <spiotrek1985@gmail.com>

---
+++ sudoku4/src/main/java/board/Board.java
@@ -0,0 +1,15 @@
+package board;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    private List<Row> board = new ArrayList<>();
+
+
+
+
+    public List<Row> getBoard() {
+        return board;
+    }
+}
